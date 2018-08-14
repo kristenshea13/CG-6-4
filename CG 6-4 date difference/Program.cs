3f18fc13a8ca5ca8cd7518c8cb39b0565906d611
Kristen Shea <kstar61@live.com>
2018-08-13 21:47:51 -0500

    *

--- CG 6-4 date difference/Program.cs
+++ CG 6-4 date difference/Program.cs
@@ -6,7 +6,21 @@ namespace CG_6_4_date_difference
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Calculate the number of days between two dates.");
+            Console.Write("Please enter first date (MM/DD/YYYY): ");
+            
+            string firstdateInput = Console.ReadLine();
+            DateTime firstDate = Convert.ToDateTime(firstdateInput);
+
+            Console.Write("Please enter second date (MM/DD/YYYY): ");
+
+            string seconddateInput = Console.ReadLine();
+            DateTime secondDate = Convert.ToDateTime(seconddateInput);
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
