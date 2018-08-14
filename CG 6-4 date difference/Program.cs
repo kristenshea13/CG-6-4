fc6a4b8da1b60a9a1a6556c5d4e8a210d5f41b08
Kristen Shea <kstar61@live.com>
2018-08-13 22:40:01 -0500

    updated to always return a positive number difference

--- CG 6-4 date difference/Program.cs
+++ CG 6-4 date difference/Program.cs
@@ -8,33 +8,35 @@ namespace CG_6_4_date_difference
         {
             Console.WriteLine("Calculate the number of days between two dates.");
             Console.Write("Please enter first date (MM/DD/YYYY): ");
-            
+
             //declare string value of user input then convert to date
             string firstdateInput = Console.ReadLine();
-            
 
-            /*discovered second way to convert to datetime from string, wanted to test it
-             so i used convert.todatetime instead of:  
-             DateTime firstDate = DateTime.Parse(firstdateInput);*/
-            DateTime firstDate = Convert.ToDateTime(firstdateInput);
+            //convert string input to date
+            DateTime firstDate = DateTime.Parse(firstdateInput);
 
-
+            //prompt for second date
             Console.Write("Please enter second date (MM/DD/YYYY): ");
 
             //declare string value of user input then convert to date.
-            //could have used parse to convert, but wanted to maintain consistency in my code
             string seconddateInput = Console.ReadLine();
-            DateTime secondDate = Convert.ToDateTime(seconddateInput);
-            
+            DateTime secondDate = DateTime.Parse(seconddateInput);
+
             //use timespan when determining an interval of time
             //take first date and subtract second date from it
             TimeSpan dayDifference = firstDate.Subtract(secondDate);
 
+            //wanted total number of days between the two to be a positive number so i used Duration
+            TimeSpan daydifferenceTotal = dayDifference.Duration();
+
+
+
             //print the difference, in days, between the two to the console.
-            Console.WriteLine(dayDifference.TotalDays);
+            Console.WriteLine(daydifferenceTotal.TotalDays);
 
 
             Console.ReadLine();
         }
+
     }
 }
