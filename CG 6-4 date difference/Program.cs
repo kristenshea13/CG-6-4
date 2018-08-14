db9af728368e723399555216eb01a65e4a01290f
Kristen Shea <kstar61@live.com>
2018-08-13 22:18:38 -0500

    calculate the number of days between two user input dates.

--- CG 6-4 date difference/Program.cs
+++ CG 6-4 date difference/Program.cs
@@ -9,15 +9,29 @@ namespace CG_6_4_date_difference
             Console.WriteLine("Calculate the number of days between two dates.");
             Console.Write("Please enter first date (MM/DD/YYYY): ");
             
+            //declare string value of user input then convert to date
             string firstdateInput = Console.ReadLine();
+            
+
+            /*discovered second way to convert to datetime from string, wanted to test it
+             so i used convert.todatetime instead of:  
+             DateTime firstDate = DateTime.Parse(firstdateInput);*/
             DateTime firstDate = Convert.ToDateTime(firstdateInput);
+
 
             Console.Write("Please enter second date (MM/DD/YYYY): ");
 
+            //declare string value of user input then convert to date.
+            //could have used parse to convert, but wanted to maintain consistency in my code
             string seconddateInput = Console.ReadLine();
             DateTime secondDate = Convert.ToDateTime(seconddateInput);
+            
+            //use timespan when determining an interval of time
+            //take first date and subtract second date from it
+            TimeSpan dayDifference = firstDate.Subtract(secondDate);
 
-
+            //print the difference, in days, between the two to the console.
+            Console.WriteLine(dayDifference.TotalDays);
 
 
             Console.ReadLine();
